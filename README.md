bfc59e5277f9b7e4985ac5b129aee7ab26f86c7d
Ruslana Solovova <ruslanasolovova@gmail.com>
2018-06-24 15:07:44 -0700

    final code of game version 1.0

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# Memory Game Project
+
+The Memory Game Project is part of the core curriculum for completing the Udacity Front-End Developer Nanodegree. 
+
+Built With:
+HTML5
+CSS3
+JavaScript - the source code uses ES6 sytax
 
 ## Table of Contents
 
@@ -6,12 +12,19 @@
 * [Contributing](#contributing)
 
 ## Instructions
+How to Play
+Go to https://github.com/neoveta/Memory-Game
+Download the repository, and open index.html in the brower.
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+Click on one card and another one on the board of sixteen mixed cards;
+If the cards match, they stay open;
+If they don't match, both of them automatically flip back face down.
+To finish the game, continue clicking on the other cards and revealing their symbol until you match all of them.
+The game starts when you click on the first card and a timer continues to count the time until all matched cards will be found.
+When you flip two cards your moves will be counted as one step. 
+If you find all eight pairs of cards less than 10 moves your star rating will be indicated as three stars. After 10 moves your star rating will be two stars, and after 15 moves and more - one star.
 
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+You can restart the game at any time by clicking the reset button situated on the upper right side of the game board.
 
 ## Contributing
 
--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
- //Create an array that holds all of cards pair
+ //Create an array that holds all of the cards pair
 let cardsArray = ["fa fa-diamond",
 "fa fa-paper-plane-o",
 "fa fa-anchor",
@@ -10,128 +10,151 @@ let cardsArray = ["fa fa-diamond",
 let cardsDeck = shuffle(cardsArray.concat(cardsArray)); //  Shuffles all cards and store them in a new array
 let openCards =[];
 let matchedCards = [];
-let stars = 3;
 let moves = 0;
 let time = 0;
-let timer;
+let updateTimer;
+let seconds = 0;
+let minutesIcon = document.getElementById('minutes');
+let secondsIcon = document.getElementById('seconds');
+secondsIcon.innerHTML = '00';
+minutesIcon.innerHTML = '00';
 let firstClick =false;
 const deck = document.querySelector(".deck");
+const nextMove = document.querySelector('.moves');
+nextMove.innerHTML =0;
+const timer = document.getElementById('timer');
 
-//new game initialization
-function startGame(){
+function startGame(){   //the new game initialization
     //create deck of cards  
-    const deck = document.querySelector(".deck");
     for (let i =0; i<cardsDeck.length; i++){
         const card = document.createElement("li");
-        card.classList.add("card"); //add class for each card in array
-        card.innerHTML = (`<i class="${cardsDeck[i]}"></i>`); //add icons class to each card
+        card.classList.add("card");     //add class for each card in array
+        card.innerHTML = (`<i class="${cardsDeck[i]}"></i>`);   //add icons class to each card
         deck.appendChild(card);
         clickEv(card); 
     }
 } 
 startGame();
 
-//click event
-function clickEv(card){   
-  //document.querySelectorAll("li.card").forEach(function(card){
-    card.addEventListener("click", function() {  //add click event for cards
-        //myTimer();
-         //open card
+//click event functionality
+function clickEv(card){  
+    card.addEventListener("click", function() {  //add click event for card
+        //if timer is not set yet - start it. Start timer at the first clik on the card
+        if (updateTimer == undefined)
+            myTimer(); 
+        //open card
         if (openCards.length ===1){
-            card.classList.add("open", "show");
+            card.classList.add("open", "show", "disable");  //"disable" prevents double click on card (pointer-events:none)
             openCards.push(card); 
-            movesCounter(); //counting the moves after two cards that were opened 
-
-                //compare two opened cards
-                if (openCards[0].innerHTML == openCards[1].innerHTML){
-                    openCards[0].classList.add("match");
-                    openCards[1].classList.add("match");
-
-                    //add mathed cards into array for gameFinished
-                    matchedCards.push(openCards[0],openCards[1]);
-
-                    openCards = []; //reset array
-                    gameOver(); //here I check if all sets card mathed and opened
-
-                } 
-                else {
-                    //Wait 800ms then : if no match - remove classes.
-                    setTimeout(function(){
-                        openCards.forEach(function(card){
-                            card.classList.remove("open", "show");
-                        });
-                        openCards = []; //reset array
-                        //TODO card.removeEventListener()
-                    }, 800);   
-                    
-                }
-
+            matchCards();   //compare two opened cards
             } 
-            else {
-            //no opened cards
-                card.classList.add("open", "show");
-                openCards.push(this); 
-                //TODO card.removeEventListener()
-            }
-            //prevent double click on card 
-            // ??? card.classList.toggle("disabled"); 
-            //prevent to open more that two cards at one step
-        });   
+        else {
+            card.classList.add("open", "show", "disable");
+            openCards.push(card); 
+        }
+    });   
+}
+//the function that compares two opened cards
+function matchCards(){
+    if (openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("match");
+        openCards[1].classList.add("match");
+        matchedCards.push(openCards[0],openCards[1]);   //add mathed cards into array for gameWon
+        openCards = [];   //reset array
+        gameWon();    //here I check if all sets card mathed and opened
+    } 
+    else {
+        setTimeout(function(){    //Wait 800ms then : if no match - remove classes
+            openCards.forEach(function(card){
+                card.classList.remove("open", "show", "disable");
+            });
+            openCards = [];   //reset array
+        }, 700);      
     }
-
-//function for "stars" functionality. Remove star related to number of steps
-function starRemove() {
-    if (moves === 10 || moves === 15 || moves === 20 ) {
-       let allStars = document.querySelectorAll("ul.stars li")
-       document.querySelector("ul.stars").removeChild(allStars[0]);         
-    }
+    movesCounter();     //counting the moves after two cards that were opened 
 }
 
-function movesCounter(){      
-    let nextMove = document.querySelector('.moves');
+function movesCounter(){      //function to count moves after two cards opened and compared
     moves++;
     nextMove.innerHTML = moves; 
     starRemove();
 }
 
+function buildStars(stars){     //the function to remove stars related to the moves
+    let allStars = document.querySelector(".stars");
+    let star = '<li><i class="fa fa-star"></i></li>';
+    let st = ''; 
+    for(var i =0; i < stars; ++i)
+        st += star;
 
+    allStars.innerHTML = st;
+}
+buildStars(3);
 
-// function myTimer(){
-//     timer = setInterval(function(){
-//     time++;
-//const minutes = Math.floor(time / 60);
-//const seconds = time % 60;
-// //console.log(time);
-//     },1000);
-// }
-// function resetTimer(){
-//     clearInterval(timer);
-// }
+function  starRemove(){
+     if(moves > 10 && moves <= 15){
+        buildStars(2);
+    } else if(moves > 15 && moves <= 20){
+        buildStars(1);
+    }    
+}
 
-let resetBtn = document.getElementById("btn");
-resetBtn.addEventListener("click", function(){
-    // resetTimer();
-    // myTimer();
+function calcTimer(seconds){
+    const minutes = Math.floor(seconds / 60);
+    const secInMin = seconds % 60;
+    return [minutes, secInMin];
+}
+
+//function to start the timer 
+function myTimer(){
+    updateTimer = setInterval(function(){  //Update timer in the screen
+            seconds++;
+            const tm = calcTimer(seconds);
+            secondsIcon.innerHTML = (tm[1] < 10) ? `0${tm[1]}` : `${tm[1]}`;
+            minutesIcon.innerHTML = (tm[0] < 10) ?  `0${tm[0]}` : `${tm[0]}`;
+    },1000);
+}
+function resetTimer(){
+    minutes = 0;
+    seconds = 0;
+    minutesIcon.innerHTML = "00";
+    secondsIcon.innerHTML = "00";
+    updateTimer = true
+}
+function stopTimer() {    //function to clearInterval to stop the timer.
+      clearInterval(updateTimer); 
+}
+
+//restart game 
+const restartIcon = document.querySelector(".restart");
+restartIcon.addEventListener("click", function(){
     moves = 0;
-    openCards = [];
-    stars = 3;  
-//remove all cards from desk - open, show, match
-//for (let i =0; i<shuffledCards.length; i++){
-    //card.classList.remove("open", "show", "match");
-//}
+    deck.innerHTML ="";    //remove all cards from desk - open, show, match
+    startGame();     //call the function to create the deck with cards
+    resetTimer();
+    nextMove.innerHTML =0;  //reset moves to start over
+    buildStars(3);
 });
 
- //create function that indicates that the game is over (all cards opened)
- function gameOver(){
-    if(matchedCards.length === cardsArray.length){ // let matchedCards =[]; if(matchedCards === 16)
-        alert("Game is Won!");
+function gameWon(){     //create function that indicates that the game is over (all cards opened and matched in deck)
+if(matchedCards.length == cardsDeck.length ){    // creates a modal when all matches have been made
+    stopTimer();    //stop timer 
+    deck.childNodes.forEach(function(card){
+        if (card.classList != undefined)
+            card.classList.remove("open", "show", "disable", "match");
+    });
+    matchedCards = [];
+    let modal = document.getElementById('myModal');  
+    modal.className=('modalshow'); //call the win modal
+    let winText=document.querySelector("#winner");
+    let allStars = document.querySelector(".stars");
+    const tm = calcTimer(seconds);
+    winText.innerHTML = "Congratulations! \nYou won the game! \nYou took "+ tm[0] + " minutes and " + tm[1] + " seconds to win the game in "+ (moves+1) + " moves. Your star rating is " + allStars.childNodes.length +".";
     }
- }
-
+}
 //shuffle the list of cards using the "shuffle" method
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -139,18 +162,5 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;  
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
